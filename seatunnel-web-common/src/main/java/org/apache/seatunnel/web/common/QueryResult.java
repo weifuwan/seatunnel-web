1692a1b2f63df40873e44ab001242bddf22a55e3
weifuwan <295227940@qq.com>
2026-05-03 19:23:10 +0800

    add count

--- seatunnel-web-common/src/main/java/org/apache/seatunnel/web/common/QueryResult.java
+++ seatunnel-web-common/src/main/java/org/apache/seatunnel/web/common/QueryResult.java
@@ -14,8 +14,25 @@ import java.util.Map;
 @NoArgsConstructor
 @ToString
 public class QueryResult implements Serializable {
+
+
+
     private List<FrontedTableColumn> columns;
+
     private List<Map<String, Object>> data;
 
-}
+    /**
+     * 当前查询条件下的数据总量。
+     *
+     * 注意：
+     * - table 模式：表示表总行数
+     * - sql 模式：表示 SQL 查询结果总行数
+     */
+    private Integer total;
 
+
+    public QueryResult(List<FrontedTableColumn> columns, List<Map<String, Object>> data) {
+        this.columns = columns;
+        this.data = data;
+    }
+}

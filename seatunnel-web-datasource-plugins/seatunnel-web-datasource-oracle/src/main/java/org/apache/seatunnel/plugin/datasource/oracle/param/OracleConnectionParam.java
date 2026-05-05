92384a66c4936f456bcecdbf6926b2b9f3b17349
weifuwan <295227940@qq.com>
2026-05-05 21:01:48 +0800

    add oracle jar

--- seatunnel-web-datasource-plugins/seatunnel-web-datasource-oracle/src/main/java/org/apache/seatunnel/plugin/datasource/oracle/param/OracleConnectionParam.java
+++ seatunnel-web-datasource-plugins/seatunnel-web-datasource-oracle/src/main/java/org/apache/seatunnel/plugin/datasource/oracle/param/OracleConnectionParam.java
@@ -1,20 +1,29 @@
 package org.apache.seatunnel.plugin.datasource.oracle.param;
 
 
+import lombok.Data;
+import lombok.EqualsAndHashCode;
 import org.apache.seatunnel.web.spi.datasource.BaseConnectionParam;
 import org.apache.seatunnel.web.spi.enums.DbConnectType;
+import org.apache.seatunnel.web.spi.form.FieldType;
+import org.apache.seatunnel.web.spi.form.FormField;
 
+@Data
+@EqualsAndHashCode(callSuper = true)
 public class OracleConnectionParam extends BaseConnectionParam {
 
     protected DbConnectType connectType;
 
-    public DbConnectType getConnectType() {
-        return connectType;
-    }
+    @FormField(label = "端口号", required = true, order = 2, defaultValue = "1521", type = FieldType.NUMBER)
+    protected String port;
 
-    public void setConnectType(DbConnectType connectType) {
-        this.connectType = connectType;
-    }
+    @FormField(
+            label = "驱动Jar包",
+            order = 6,
+            defaultValue = "ojdbc8-19.3.0.0.jar"
+    )
+    protected String driverLocation;
+
 
     @Override
     public String toString() {
